6d017f8f049748f96ee1dcce67d5ec6314210f41
Lahiru Jayasuriya <chamathjayasuriya2002cmp@gmail.com>
2026-05-06 10:06:54 +0530

    Upload the file to the repo

---
+++ playwright.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  use: {
+    headless: false,
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    actionTimeout: 15000
+  },
+  reporter: [['html', { open: 'always' }]]
+});
