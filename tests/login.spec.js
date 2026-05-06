69337fc51cc8de51b4e3f41530f996c232d3bc58
Lahiru Jayasuriya <chamathjayasuriya2002cmp@gmail.com>
2026-05-06 11:34:31 +0530

    Implement SideBar class and update login tests to utilize sidebar navigation

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,27 @@
 const { test, expect } = require('@playwright/test');
 const LoginPage = require('../pages/LoginPage');
+const SideBar = require('../pages/SideBar');
 
 test('Valid Login Test', async ({ page }) => {
     const login = new LoginPage(page);
+    const sidebar = new SideBar(page);
 
     await login.goto();
     await login.login('superadmin@shopbox.com', 'Admin@123');
 
     await expect(page).toHaveURL(/dashboard/);
+    
+    await sidebar.clickDashboard();
+    await sidebar.clickSales();
+    await sidebar.clickProducts();
+    await sidebar.clickPurchases();
+    await sidebar.clickCustomer();
+    await sidebar.clickUsers();
+    await sidebar.clickReports();
+    await sidebar.clickSettings();
+    
+
+    
+    
 });
 
